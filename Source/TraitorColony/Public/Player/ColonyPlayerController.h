21988bf6066102bb365171f149e5895efea0974d
Connor Priest <Kon_Priest@Hotmail.com>
2024-12-11 16:24:57 +0000

    Setup base game mode and player character/controller

---
+++ Source/TraitorColony/Public/Player/ColonyPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ColonyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAITORCOLONY_API AColonyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
